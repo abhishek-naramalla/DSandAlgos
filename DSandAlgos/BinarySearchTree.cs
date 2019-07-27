0bb139a3ab2c0307d0596eabad97ae16b375a587
Abhis <nabhis501@gmail.com>
2019-07-27 16:34:56 -0700

    Adding BST
    Adding BST..

---
+++ DSandAlgos/BinarySearchTree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace DSandAlgos
+{
+    public class BinarySearchTree
+    {
+
+        public class Node {
+           public int data;
+           public Node left;
+           public Node right;
+            public Node(int i)
+            {
+                data = i;
+            }
+        }
+        Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+
+        }
+
+        public void Insert(int value)
+        {
+            var newNode = new Node(value);
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (value < current.data)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = newNode; //since current is null - use parent
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
---
+++ DSandAlgos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSandAlgos
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
