b5e11c3baedbf33bbbf2971eba9ae08a0048a8e7
Abhis <nabhis501@gmail.com>
2019-07-27 16:54:42 -0700

    minor changes..

--- DSandAlgos/BinarySearchTree.cs
+++ DSandAlgos/BinarySearchTree.cs
@@ -43,6 +43,15 @@ namespace DSandAlgos
                             break;
                         }
                     }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = newNode; //since current is null - use parent
+                            break;
+                        }
+                    }
                 }
             }
 
