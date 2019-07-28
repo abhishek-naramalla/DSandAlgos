618711d95b5847134257dcd4045506237d78669f
Abhis <nabhis501@gmail.com>
2019-07-27 17:01:06 -0700

    minor comments..

--- DSandAlgos/Program.cs
+++ DSandAlgos/Program.cs
@@ -6,6 +6,11 @@ namespace DSandAlgos
     {
         public static void Main(string[] args)
         {
+            var bst = new BinarySearchTree();
+            bst.Insert(1);
+            bst.Insert(2);
+            bst.Insert(3);
+
             Console.WriteLine("Hello World!");
         }
     }
